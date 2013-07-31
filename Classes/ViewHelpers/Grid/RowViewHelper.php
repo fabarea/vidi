348b879801f41eecfa791538312e7167493b206a
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-07-31 11:31:43 +0200

    [TASK] Clean up old View Helper
    Releases: 1.0.0
    Fixes: #50583

--- Classes/ViewHelpers/Grid/RowViewHelper.php
+++ Classes/ViewHelpers/Grid/RowViewHelper.php
@@ -25,11 +25,6 @@ namespace TYPO3\CMS\Vidi\ViewHelpers\Grid;
 
 /**
  * View helper for rendering rows of contents
- *
- * @category    ViewHelpers
- * @package     TYPO3
- * @subpackage  media
- * @author      Fabien Udriot <fabien.udriot@typo3.org>
  */
 class RowViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
