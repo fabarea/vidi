b625e9ae1f0015c5338ff99a9a79a4284ab12efd
Steffen Müller <typo3@t3node.com>
2014-03-12 19:18:29 +0100

    [BUGFIX] Skip ModuleMenuView override for 6.2
    The functionality is part of 6.2
    so the XCLASS is not required
    for that version.
    
    Resolves: #56392
    Change-Id: I690a258b1e9d52b4d7f37bb389165b9a0e46f0b0

--- Classes/Override/Backend/View/ModuleMenuView.php
+++ Classes/Override/Backend/View/ModuleMenuView.php
@@ -73,69 +73,7 @@ class ModuleMenuView extends \TYPO3\CMS\Backend\View\ModuleMenuView {
 	 * @return array Multi dimension array with module data
 	 */
 	public function getRawModuleData() {
-		$modules = array();
-		// Remove the 'doc' module?
-		if ($GLOBALS['BE_USER']->getTSConfigVal('options.disableDocModuleInAB')) {
-			unset($this->loadedModules['doc']);
-		}
-
 		$this->unsetHiddenModules();
-
-		foreach ($this->loadedModules as $moduleName => $moduleData) {
-			$moduleLink = '';
-			if (!is_array($moduleData['sub'])) {
-				$moduleLink = $moduleData['script'];
-			}
-			$moduleLink = \TYPO3\CMS\Core\Utility\GeneralUtility::resolveBackPath($moduleLink);
-			$moduleKey = 'modmenu_' . $moduleName;
-			$moduleIcon = $this->getModuleIcon($moduleKey);
-			$modules[$moduleKey] = array(
-				'name' => $moduleName,
-				'title' => $GLOBALS['LANG']->moduleLabels['tabs'][$moduleName . '_tab'],
-				'onclick' => 'top.goToModule(\'' . $moduleName . '\');',
-				'icon' => $moduleIcon,
-				'link' => $moduleLink,
-				'description' => $GLOBALS['LANG']->moduleLabels['labels'][$moduleKey . 'label']
-			);
-			if (!is_array($moduleData['sub']) && $moduleData['script'] != 'dummy.php') {
-				// Work around for modules with own main entry, but being self the only submodule
-				$modules[$moduleKey]['subitems'][$moduleKey] = array(
-					'name' => $moduleName,
-					'title' => $GLOBALS['LANG']->moduleLabels['tabs'][$moduleName . '_tab'],
-					'onclick' => 'top.goToModule(\'' . $moduleName . '\');',
-					'icon' => $this->getModuleIcon($moduleName . '_tab'),
-					'link' => $moduleLink,
-					'originalLink' => $moduleLink,
-					'description' => $GLOBALS['LANG']->moduleLabels['labels'][$moduleKey . 'label'],
-					'navigationFrameScript' => NULL,
-					'navigationFrameScriptParam' => NULL,
-					'navigationComponentId' => NULL
-				);
-			} elseif (is_array($moduleData['sub'])) {
-				foreach ($moduleData['sub'] as $submoduleName => $submoduleData) {
-					$submoduleLink = \TYPO3\CMS\Core\Utility\GeneralUtility::resolveBackPath($submoduleData['script']);
-					$submoduleKey = $moduleName . '_' . $submoduleName . '_tab';
-					$submoduleIcon = $this->getModuleIcon($submoduleKey);
-					$submoduleDescription = $GLOBALS['LANG']->moduleLabels['labels'][$submoduleKey . 'label'];
-					$originalLink = $submoduleLink;
-					$modules[$moduleKey]['subitems'][$submoduleKey] = array(
-						'name' => $moduleName . '_' . $submoduleName,
-						'title' => $GLOBALS['LANG']->moduleLabels['tabs'][$submoduleKey],
-						'onclick' => 'top.goToModule(\'' . $moduleName . '_' . $submoduleName . '\');',
-						'icon' => $submoduleIcon,
-						'link' => $submoduleLink,
-						'originalLink' => $originalLink,
-						'description' => $submoduleDescription,
-						'navigationFrameScript' => $submoduleData['navFrameScript'],
-						'navigationFrameScriptParam' => $submoduleData['navFrameScriptParam'],
-						'navigationComponentId' => $submoduleData['navigationComponentId']
-					);
-					if ($moduleData['navFrameScript']) {
-						$modules[$moduleKey]['subitems'][$submoduleKey]['parentNavigationFrameScript'] = $moduleData['navFrameScript'];
-					}
-				}
-			}
-		}
-		return $modules;
+		return parent::getRawModuleData();
 	}
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,8 +6,10 @@ if (!defined('TYPO3_MODE')) {
 // Setting up scripts that can be run from the cli_dispatch.phpsh script.
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'TYPO3\CMS\Vidi\Command\VidiCommandController';
 
-// Override classes for the Object Manager
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Backend\View\ModuleMenuView'] = array(
-	'className' => 'TYPO3\CMS\Vidi\Override\Backend\View\ModuleMenuView'
-);
+// Override classes for the Object Manager. Can be removed once 6.1 has reached EOL.
+if (version_compare(TYPO3_branch, '6.1', '<=')) {
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Backend\View\ModuleMenuView'] = array(
+		'className' => 'TYPO3\CMS\Vidi\Override\Backend\View\ModuleMenuView'
+	);
+}
 ?>
