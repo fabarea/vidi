ba1c3b6b6c782ada45805c8a393ada224b88aad8
Bodo Eichstädt <be@typo3cluster.com>
2015-03-04 10:52:24 +0100

    safeguard if EXT:scheduler is not loaded
    if EXT:scheduler is not loaded, then the call mergeRecursiveWithOverrule will render Frontend AND Backend broken

--- Configuration/TCA/Overrides/tx_scheduler_task_group.php
+++ Configuration/TCA/Overrides/tx_scheduler_task_group.php
@@ -1,12 +1,14 @@
 <?php
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
-$tca = array(
-	'vidi' => array(
-		'mappings' => array(
-			'groupName' => 'groupName'
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('scheduler')) {
+	$tca = array(
+		'vidi' => array(
+			'mappings' => array(
+				'groupName' => 'groupName'
+			)
 		)
-	)
-);
-
-\TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA']['tx_scheduler_task_group'], $tca);
+	);
+	
+	\TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA']['tx_scheduler_task_group'], $tca);
+}
