1c29e589a34a471dd8ff41d47b39d8f2969f47dd
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-10-14 18:37:42 +0200

    [TASK] Prepare next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => '0',
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.1.1',
+  'version' => '1.2.0-dev',
   'constraints' =>
   array (
     'depends' =>
