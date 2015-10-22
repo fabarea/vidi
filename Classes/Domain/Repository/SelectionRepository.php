45349b8e753daced358f743b27fb4b39b12bc28c
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-10-22 11:51:55 +0200

    [FEATURE] Add method to retrieve "public" Selection
    Resolves #75

--- Classes/Domain/Repository/SelectionRepository.php
+++ Classes/Domain/Repository/SelectionRepository.php
@@ -71,6 +71,29 @@ class SelectionRepository extends Repository {
 	}
 
 	/**
+	 * @param string $dataType
+	 * @return QueryResult
+	 */
+	public function findForEveryone($dataType) {
+		$query = $this->createQuery();
+
+		// Add matching criteria
+		$query->matching(
+			$query->logicalAnd(
+				$query->equals('dataType', $dataType),
+				$query->equals('visibility', Selection::VISIBILITY_EVERYONE)
+			)
+		);
+
+		// Set ordering
+		$query->setOrderings(
+			array('name' => QueryInterface::ORDER_ASCENDING)
+		);
+
+		return $query->execute();
+	}
+
+	/**
 	 * Returns an instance of the current Backend User.
 	 *
 	 * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
