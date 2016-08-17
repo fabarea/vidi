c4fc19671e092040fc2b653c3c41bf5df4449307
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-08-17 16:34:21 +0200

    [TASK] Update email in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,14 +5,13 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Generic listing of records with versatile ways of interacting with the data, e.g. advanced filter, inline editing, mass editing, ... Veni, vidi, vici!',
     'category' => 'module',
     'author' => 'Fabien Udriot',
-    'author_email' => 'fabien.udriot@typo3.org',
+    'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'stable',
     'version' => '2.3.0-dev',
-    'autoload' => 
-		[
-            		'psr-4' => ['Fab\\Vidi\\' => 'Classes']
-		],
+    'autoload' => [
+        'psr-4' => ['Fab\\Vidi\\' => 'Classes']
+    ],
     'constraints' =>
         [
             'depends' =>
