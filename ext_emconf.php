4eeae5bd840592aa4d227d196704c1088c8dad15
Fabien Udriot <fabien@omic.ch>
2019-01-10 10:31:52 +0100

    Update extension version to 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'stable',
-    'version' => '2.8.0-dev',
+    'version' => '3.0.0',
     'autoload' => [
         'psr-4' => ['Fab\\Vidi\\' => 'Classes']
     ],
