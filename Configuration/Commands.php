a607f109e30406782ecaa93193f92c2f7ca180b7
Fabien Udriot <fabien@omic.ch>
2019-04-22 21:05:44 +0200

    Fix command name in comments

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * Commands to be executed by typo3, where the key of the array
- * is the name of the command (to be called as the first argument after typo3).
- * Required parameter is the "class" of the command which needs to be a subclass
- * of Symfony/Console/Command.
- *
- * example: bin/typo3 backend:lock
+ * example: bin/typo3 vidi:analyseRelations
  */
 return [
     'vidi:analyseRelations' => [
