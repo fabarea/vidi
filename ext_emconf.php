1c334345d27091e14da871e86922f78edf6aa6c8
Fabien Udriot <fabien@omic.ch>
2019-05-02 18:34:32 +0200

    Release 3.2.1 - <https://github.com/fabarea/vidi/compare/3.2.0...3.2.1>
    [BUGFIX] Fix return type

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'stable',
-    'version' => '3.3.0-dev',
+    'version' => '3.2.1',
     'autoload' => [
         'psr-4' => ['Fab\\Vidi\\' => 'Classes']
     ],
