9e577214cfaaf5a8d1e81fd4f79b0086f47d8724
Fabien Udriot <fabien@omic.ch>
2020-06-05 09:51:02 +0200

    Release 4.0.0 - <https://github.com/fabarea/vidi/compare/3.3.2...4.0.0>
    [BUGFIX] Fix ObjectManager injection
    [TASK] Use Doctrine API everywhere
    [TASK] Use Doctrine API in VidiDbBackend

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'stable',
-    'version' => '3.4.0-dev',
+    'version' => '4.0.0',
     'autoload' => [
         'psr-4' => ['Fab\\Vidi\\' => 'Classes']
     ],
