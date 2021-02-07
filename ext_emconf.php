7b91d75615880262bfd0a23a2661cd07bac14b55
Fabien Udriot <fabien@omic.ch>
2021-02-08 00:30:30 +0100

    [TASK] Prepare next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'stable',
-    'version' => '4.0.1',
+    'version' => '4.1.0-dev',
     'autoload' => [
         'psr-4' => ['Fab\\Vidi\\' => 'Classes']
     ],
