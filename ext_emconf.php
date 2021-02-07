c91a647dd2f054a1f0c440f321a7d4341a92e70a
Fabien Udriot <fabien@omic.ch>
2021-02-08 00:01:42 +0100

    Release 4.0.1 - <https://github.com/fabarea/vidi/compare/4.0.0...4.0.1>

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'stable',
-    'version' => '4.0.0-dev',
+    'version' => '4.0.1',
     'autoload' => [
         'psr-4' => ['Fab\\Vidi\\' => 'Classes']
     ],
