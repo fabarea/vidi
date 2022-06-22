c3de37c7263633cdbc9b0ec803b11785fcbbae2a
Fabien Udriot <fabien@omic.ch>
2022-06-22 12:07:21 +0200

    Release 5.0.3 - <https://github.com/fabarea/vidi/compare/5.0.2...5.0.3>

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## 5.0.3 (2022-06-22)
+
+[BUGFIX] Remove call to deprecated workspace attribute
+[BUGFIX] Correctly initialize storage as memory cache
+[BUGFIX] TYPO3 dependency
+[TASK] Prepare next release
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,25 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Versatile and Interactive Display - List Component',
     'description' => 'Generic listing of records with versatile ways of interacting with the data, e.g. advanced filter, inline editing, mass editing, ... Veni, vidi, vici!',
-    'category' => 'module',
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
-    'version' => '5.1.0-dev',
-    'autoload' => [
-        'psr-4' => ['Fab\\Vidi\\' => 'Classes']
-    ],
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '11.5.0-11.5.99',
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                ],
-        ]
 ];
